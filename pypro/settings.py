872bc9bffa1a790cc5597b6dec3ff6c31cb56333
root <root@DESKTOP-J796AAE.localdomain>
2019-12-16 14:49:42 -0300

    Language code modificado

--- pypro/settings.py
+++ pypro/settings.py
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = config('LANGUAGE_CODE', default='pt-br')
+LANGUAGE_CODE = config('LANGUAGE_CODE', default='en-us')
 
 TIME_ZONE = config('TIME_ZONE', default='America/Sao_Paulo')
 
